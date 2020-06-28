d841eb0107b5d54e648255e9aa37c0d85a623467
floydluo <floydjjluo@gmail.com>
2020-06-28 17:28:01 +0800

    update sentence

--- nlptext/sentence.py
+++ nlptext/sentence.py
@@ -12,8 +12,8 @@ from .utils.infrastructure import START, END, START_ID, END_ID, UNK_ID
 import re 
 
 
-START_TK = Token(token = START)
-END_TK   = Token(token = END)
+# START_TK = Token(token = START)
+# END_TK   = Token(token = END)
 
 class Sentence(BasicObject):
     _sentence   = None
@@ -51,7 +51,7 @@ class Sentence(BasicObject):
         try:
             return [bioes2tag[vocidx] for vocidx in grain_idx]
         except:
-            print(grain_idx)
+            print('://nlptext.Sentence.get_stored_hypertagscheme', grain_idx)
 
     def get_stored_hyperstring(self, channel, tagScheme):
         vocidx2grain = self.getGrainVocab(channel, tagScheme = tagScheme)[0]
@@ -82,15 +82,24 @@ class Sentence(BasicObject):
             # sent = self.sentence.split(' ')
             tokenLevel = self._tokenLevel if self._tokenLevel else self.TOKEN['TOKENLevel']
             # the sentence input is separated by spaces
+            # instead of using lookup table, it run the function directly.
             # print(tagScheme)
             channelGrain =  getChannelGrain4Sent(self.sentence, channel, Min_Ngram = Min_Ngram, Max_Ngram = Max_Ngram, 
                                                  end_grain = end_grain, tagScheme  = tagScheme, tokenLevel = tokenLevel)
 
             return channelGrain
 
-    def get_grain_idx(self, channel, Min_Ngram = 1, Max_Ngram = 1, end_grain = False, min_grain_freq = 1, tagScheme = 'BIO', channel_name = None, 
+    def get_grain_idx(self, channel,    
+                      # series1.
+                      Min_Ngram = 1, Max_Ngram = 1, end_grain = False, min_grain_freq = 1, tagScheme = 'BIO', 
+                      # series2. -- (channel_name is the abrr for series1)
+                      channel_name = None,  
+                      # series3. -- (where to get GU, TU, LKP, and TRANS, not frequently used, associated with "series1" and "Corpus")
                       Data_Dir = None,
-                      GU = None, TU = None, LKP = None, TRANS = None, unk_id = None):
+                      # series4. -- (actually, associated with "series1" and "Corpus")
+                      GU = None, TU = None, LKP = None, TRANS = None, 
+                      # important items.
+                      unk_id = None, tk_unk_id = None, mask_unk_tk_info = False):
 
         '''
             1. Once GU TU LKP and TRANS are settled, channel_name is settled before.
@@ -108,11 +117,21 @@ class Sentence(BasicObject):
                 if not Data_Dir:
                     TU = self.TokenVocab; GU = TU
                 else:
+                    # print('[nlptext.Sentence.get_grain_idx]:', 'get its own GU')
                     TU = self.getGrainVocab('token', Data_Dir = Data_Dir); GU = TU
 
             LTU, DTU = TU
-            if unk_id is None: unk_id = len(DTU)
-            info = [[DTU.get(tk, unk_id)] for tk in self.sentence.split(' ')]
+            flag = False
+            if tk_unk_id is None: 
+                flag = True
+                tk_unk_id = len(DTU)
+                
+            info = [[DTU.get(tk, tk_unk_id)] for tk in self.sentence.split(' ')]
+
+            # check whether the unk token in current sentence.
+            if [tk_unk_id] in info and flag:
+                print('[nlptext.Sentence.get_grain_idx]: temporary unk_id for (', channel, ') is:', tk_unk_id)
+
             leng_st = len(info)
             leng = [1] * leng_st
             return info, leng_st, leng, 1
@@ -127,9 +146,20 @@ class Sentence(BasicObject):
         
         if channel in self.Channel_Hyper_Path:
             # deal with hyper fields
-            if not GU: GU = self.getGrainVocab(channel, channel_name = channel_name, Data_Dir =Data_Dir)
+            if not GU: 
+                GU = self.getGrainVocab(channel, channel_name = channel_name, Data_Dir =Data_Dir)
             LGU, DGU = GU
-            if unk_id is None: unk_id = len(DGU)
+
+            # if unk_id is None: 
+            #     print('[nlptext.Sentence.get_grain_idx]: temporary unk_id for (', channel, ') is:', len(unk_id))
+            #     unk_id = len(DGU)
+
+            if unk_id is None: 
+                unk_id = len(LGU)
+                padding_idx = unk_id + 1
+            else:
+                padding_idx = 0
+
 
             if not self._sentence:
                 # Generally, we will use this one.
@@ -140,17 +170,36 @@ class Sentence(BasicObject):
                 leng = [1] * leng_st
                 return info, leng_st, leng, 1
             else:
+                print('[nlptext.Sentence.get_grain_idx]: get hyper fields by functions:', tk_unk_id)
                 info = self.get_grain_str(channel, channel_name = channel_name)
                 # print(info)
                 info = [[DGU.get(gr, unk_id) for gr in tk] for tk in info]
-                info, leng_st, leng_tk, max_gr = self.padding_info(info)
+                info, leng_st, leng_tk, max_gr = self.padding_info(info, padding_idx = padding_idx)
                 return info, leng_st, leng_tk, max_gr
 
-        else: 
+        elif channel in self.CONTEXT_IND_CHANNELS: 
             # deal with sub fields
-            if not GU: GU = self.getGrainVocab(channel, channel_name = channel_name, Data_Dir =Data_Dir)
+            # if GU.
+            #   if TU, LKP: 
+            #     for token_str in sentence_str.
+            #       if token in TU:
+            #         token_str ---(TU)-->     token_idx      --------(LKP)-------> [..., gr_idx, ...]
+            #
+            #       elif token not in TU:  #(OOV)
+            #         token_str --(Func)-> [..., gr_str, ...] --(GU, gr_unk_idx)--> [..., gr_idx, ...]
+            
+            if not GU: 
+                GU = self.getGrainVocab(channel, channel_name = channel_name, Data_Dir =Data_Dir)
+
             LGU, DGU = GU
-            if unk_id is None: unk_id = len(LGU)
+
+            flag1 = False
+            if unk_id is None: 
+                unk_id = len(LGU)
+                flag1 = True
+                padding_idx = unk_id + 1
+            else:
+                padding_idx = 0
 
             if not (LKP and TU):
                 try:
@@ -162,41 +211,67 @@ class Sentence(BasicObject):
                 # if we can get LKP and TU
                 sentence_tokens = self.sentence.split(' ')
                 LTU, DTU = TU
-                if unk_id is None: 
+                
+                flag2 = False
+                if tk_unk_id is None: 
                     tk_unk_id = len(DTU)
-                else:
-                    tk_unk_id = unk_id # NOTICE: we assume that both grain_unkown and token unknown are the same.
+                    tk_padding_idx = tk_unk_id + 1
+                    flag2 = True
 
                 tk_voc_info = [DTU.get(tk, tk_unk_id) for tk in sentence_tokens]
-                # print(tk_voc_info)
 
                 info = []
                 for idx, tk_voc in enumerate(tk_voc_info):
                     if tk_voc != tk_unk_id:
                         info.append(LKP[tk_voc])
-                    else:
+                    elif mask_unk_tk_info == False:
                         # deal with the low freq words
                         token_str = sentence_tokens[idx]
-                        print(token_str) # TODO: remove this to log
+                        # print('[nlptext.Sentence.get_grain_idx]: get grain idx for the OOV token: [', token_str, '] for field', channel) 
+                        if flag2:
+                            print('[nlptext.Sentence.get_grain_idx]: temporary unk_id for ( token ) is:', tk_unk_id)
                         grains = getChannelGrain4Token(token_str, channel, Min_Ngram = Min_Ngram, Max_Ngram = Max_Ngram, end_grain = end_grain)
-                        info.append([DGU.get(gr, unk_id) for gr in grains])
+                        gr_list = [DGU.get(gr, unk_id) for gr in grains]
+                        if unk_id in gr_list and flag1:
+                            print('[nlptext.Sentence.get_grain_idx]: temporary unk_id for (', channel, ') is:', unk_id)
+                        info.append(gr_list)
+                    elif mask_unk_tk_info == True:
+                        token_str = sentence_tokens[idx]
+                        print('[nlptext.Sentence.get_grain_idx]: get grain idx for the OOV token: [', token_str, '] for field', channel) 
+                        print("[nlptext.Sentence.get_grain_idx]: mask the unk token's grain information")
+                        info.append([unk_id])
+                    else:
+                        raise ValueError("[nlptext.Sentence.get_grain_idx]: Error")
 
-                info, leng_st, leng_tk, max_gr = self.padding_info(info)
+                # assert 
+                info, leng_st, leng_tk, max_gr = self.padding_info(info, padding_idx = padding_idx)
                 return info, leng_st, leng_tk, max_gr
+
             else:
                 # if we cannot get LKP and TU
                 info = self.get_grain_str(channel, channel_name = channel_name)
                 info = [[DGU.get(gr, unk_id) for gr in tk] for tk in info]
-                info, leng_st, leng_tk, max_gr = self.padding_info(info)
+                
+                # check have gr_unk_idx or not.
+                for gr_list in info:
+                    if unk_id in gr_list:
+                        print('[nlptext.Sentence.get_grain_idx]/no TU and LKP: temporary unk_id for (', channel, ') is:', unk_id)
+                        break
+
+                info, leng_st, leng_tk, max_gr = self.padding_info(info, padding_idx = padding_idx)
                 return info, leng_st, leng_tk, max_gr
 
-    
-    def padding_info(self, info, leng_tk = None):
+        else:
+            print('[nlptext.Sentence.get_grain_idx]: Errors') 
+
+    def padding_info(self, info, leng_tk = None, padding_idx = 0):
         if not leng_tk: leng_tk = [len(i) for i in info]
         leng_st = len(info)
         max_gr  = max(leng_tk)
         info_final =np.zeros([leng_st, max_gr], dtype=int)
-        info = [tk + [0] * (max_gr - len(tk)) for tk in info] 
+        # TODO: 0 could be the first items.
+        # The GU first item should be </pad>.
+        info = [tk + [padding_idx] * (max_gr - len(tk)) for tk in info] 
         return info, leng_st, leng_tk, max_gr
         
     @property 
